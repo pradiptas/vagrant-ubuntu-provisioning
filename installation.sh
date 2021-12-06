923182cc25f821f1cf416bbbb4bba27208ac17b9
Pradipta Sanyal <pradipta.sanyal@gmail.com>
2021-12-05 23:17:45 -0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -9,6 +9,7 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+#install docker engine
 yes | sudo apt-get install docker-ce docker-ce-cli containerd.io
 sleep 120
 # add user to docker group
